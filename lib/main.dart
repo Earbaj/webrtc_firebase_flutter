322465caaaef895521baeb2e64b69647318fb308
Earbaj <earbajsaria3@gmail.com>
2026-01-18 12:13:38 +0600

    initialize firebase to main and main structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/themes/app_theme.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,20 +9,29 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter WebRTC Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SizedBox()
+  Widget build(BuildContext context, WidgetRef ref) {
+    //final router = ref.watch(appRouterProvider);
+
+    return MaterialApp.router(
+      title: 'WebRTC Video Call',
+      theme: AppTheme.darkTheme,
+      //routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaleFactor: 1.0, // Prevent system font scaling
+          ),
+          child: child!,
+        );
+      },
     );
   }
 }
